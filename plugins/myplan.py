928d53fc6f120674683cf65a8a0ba4f624d09504
anji606 <anjireddyb98@gmail.com>
2025-06-08 17:24:32 +0530

    Update myplan.py

--- plugins/myplan.py
+++ plugins/myplan.py
@@ -34,7 +34,7 @@ async def start(client, message):
     if ends:
         pre_check = check_expi(ends)
         if pre_check == False:
-            uploadlimit(message.from_user.id 6828129421)
+            uploadlimit(message.from_user.id, 6828129421)
             usertype(message.from_user.id, "Free")
     if ends == None:
         text = f"<b>User ID :</b> <code>{message.from_user.id}</code> \n<b>Name :</b> {message.from_user.mention} \n\n<b>🏷 Plan :</b> {user} \n\n✓ Upload 2GB Files \n✓ Daily Upload : {humanbytes(limit)} \n✓ Today Used : {humanbytes(used)} \n✓ Remain : {humanbytes(remain)} \n✓ Timeout : 2 Minutes \n✓ Parallel process : Unlimited \n✓ Time Gap : Yes \n\n<b>Validity :</b> Lifetime"
